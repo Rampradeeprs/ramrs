a34e7921cb4bcc3123804de52d5988e64185ca0f
ram pradeep <30342988+Rampradeeprs@users.noreply.github.com>
2017-07-24 12:18:34 +0600

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,n;
+printf("enter thevalue");
+scanf("%d",&n);
+for(i=1;i<15;i++)
+{
+if(i%2!=0)
+{
+printf("the odd numbers are");
+}
+}
+
+
