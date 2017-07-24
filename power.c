4a076cc5129e5b2f4522ee149f53466c99df53e7
ram pradeep <30342988+Rampradeeprs@users.noreply.github.com>
2017-07-24 10:11:40 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int pow,exp,res=1;
+printf("enter the elements");
+scanf("%d%d",&pow,&exp);
+{
+while(exp!=0)
+{
+res=(res*base);
+exp--;
+printf("the result is");
+}
+}
