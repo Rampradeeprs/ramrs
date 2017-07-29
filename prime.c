874beda6aca3af456d6b5f60a2c052ffb5d0e64c
ram pradeep <30342988+Rampradeeprs@users.noreply.github.com>
2017-07-29 17:01:30 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+int n,i,flag=0;
+printf("enter the number");
+scanf("%d",&n);
+for(i=2;i<=n/2;i++)
+{
+if(n/i==)
+{
+flag=1;
+break;
+}
+{
+if(n==0)
+{
+printf("it is prime);
+}
+else
+{
+printf(it is not prime);
+}
+}
