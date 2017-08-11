ec43ec8c191ef67e491409c1b7bc262edd087489
ram pradeep <30342988+Rampradeeprs@users.noreply.github.com>
2017-08-11 21:57:40 +0530

    Create genmulti.c

---
+++ genmulti.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int n,i,range;
+printf("enter the integer");
+scanf("%d",&i);
+printf("enter the range");
+scanf("%d",&range);
+for(i=0;i<=range;i++)
+{
+printf("%d*%d=%d,n.i.n*i");
+}
+}
