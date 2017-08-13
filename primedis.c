ecb71be9640d1f8c0b6b4c33140e06d520f52bb3
ram pradeep <30342988+Rampradeeprs@users.noreply.github.com>
2017-08-13 12:32:42 +0530

    Create new file

---
+++ primedis.c
@@ -0,0 +1,22 @@
+#includew<stdio.h>
+void main()
+{
+int n,i,flag;
+printf("enter the number");
+scanf("%d",&n);
+for(i=0;i<20;i++)
+{
+for(i=2;i=n%2;i++)
+{
+if(n%2==0)
+{
+flag=1;
+break;
+}
+{
+if(n==1)
+{
+printf("the primr numbers are");
+}
+}
+}
